cd1149124ad43ecffe8b4fc90d290affd3594a13
LaraDebsi <ldebs082@uottawa.ca>
2025-03-19 13:02:58 -0400

    Creating a frontend via REACT

--- src/main/java/com/example/demo/Booking.java
+++ src/main/java/com/example/demo/Booking.java
@@ -4,16 +4,27 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "booking", schema = "hotel")
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "booking_ID")
     private Long id;
     private String status;
 
+    @ManyToOne
+    @JoinColumn(name = "customer_ID", nullable = false)
     private Long customerId;
+
+    @ManyToOne
+    @JoinColumn(name = "room_ID", nullable = false)
     private Long roomId;
+
+    @Column(name = "start_date")
     private LocalDate startDate;
+
+    @Column(name = "end_date")
     private LocalDate endDate;
 
     // Getters and Setters
